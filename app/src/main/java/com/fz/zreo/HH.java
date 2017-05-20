e830fcdc3ea606eabcff24618c72ff7c2d257828
Zero <f1178776647@qq.com>
2017-05-20 10:38:19 +0800

    fuzhuo

---
+++ app/src/main/java/com/fz/zreo/HH.java
@@ -0,0 +1,16 @@
+package com.fz.zreo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zero on 2017/5/20.
+ */
+
+public class HH extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abc_search_view);
+    }
+}
