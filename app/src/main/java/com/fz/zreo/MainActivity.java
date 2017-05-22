6f121f71ae117fa1dca9bad94b9a19b473a7d221
Zero <f1178776647@qq.com>
2017-05-22 15:36:55 +0800

    fuzhuo

--- app/src/main/java/com/fz/zreo/MainActivity.java
+++ app/src/main/java/com/fz/zreo/MainActivity.java
@@ -37,18 +37,9 @@ public class MainActivity extends Activity {
     private void setLeftMenu() {
         menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT);
-        // 设置触摸屏幕的模式
         menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        //menu.setShadowWidthRes(R.dimen.shadow_width);
-        //menu.setShadowDrawable(R.color.colorAccent);
-
-        // 设置滑动菜单视图的宽度
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        // 设置渐入渐出效果的值
-        menu.setFadeDegree(0.35f);
-
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        //为侧滑菜单设置布局
         menu.setMenu(R.layout.left_menu);
     }
 
