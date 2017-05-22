e72146298382fc78a8586626ac07e430b3b460db
Zero <f1178776647@qq.com>
2017-05-22 15:30:57 +0800

    fuzhuo

--- app/src/main/java/com/fz/zreo/MainActivity.java
+++ app/src/main/java/com/fz/zreo/MainActivity.java
@@ -3,6 +3,8 @@ package com.fz.zreo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
@@ -11,17 +13,34 @@ import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 public class MainActivity extends Activity {
     private long mExitTime;
     private SlidingMenu menu;
+    private TextView tvLeftMenuCXczhcz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setLeftMenu();
+        initView();
+    }
+
+    private void initView() {
+        tvLeftMenuCXczhcz = (TextView) findViewById(R.id.tv_leftmenu_xczhcz);
+        tvLeftMenuCXczhcz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = tvLeftMenuCXczhcz.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setLeftMenu() {
         menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT);
         // 设置触摸屏幕的模式
         menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.color.colorAccent);
+        //menu.setShadowWidthRes(R.dimen.shadow_width);
+        //menu.setShadowDrawable(R.color.colorAccent);
 
         // 设置滑动菜单视图的宽度
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
